6f88c11786e8a2867508e3432c421a22c3aa1f49
EuanGeleziunas <123413919+EuanGeleziunas@users.noreply.github.com>
2023-05-21 18:14:41 +0100

    Get User request implemented on profile screen.

--- WhatsThatClientProject/screens/profileScreen.jsx
+++ WhatsThatClientProject/screens/profileScreen.jsx
@@ -2,34 +2,56 @@
 /* eslint-disable no-unused-vars */
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ActivityIndicator } from 'react-native';
 
 export default class ProfileScreen extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      // firstName: '',
-      // lastName: '',
-      // email: '',
+      firstName: '',
+      lastName: '',
+      email: '',
       // error: '',
-      // isLoading: '',
+      isLoading: true,
     };
   }
 
   componentDidMount() {
     this.unsubscribe = this.props.navigation.addListener('focus', () => {
       if (this.checkLoggedIn()) {
-        // this.getProfileData;
+        this.getUserRequest();
       }
     });
-
-    // this.getData();
   }
 
   componentWillUnmount() {
     this.unsubscribe();
   }
+
+  getUserRequest = async () => {
+    try {
+      const id = await AsyncStorage.getItem('userId');
+      const authToken = await AsyncStorage.getItem('sessionAuthToken');
+      const response = await fetch(`http://localhost:3333/api/1.0.0/user/${id}`, {
+        headers: {
+          'X-Authorization': `${authToken}`,
+        },
+      });
+      const json = await response.json();
+
+      this.setState({
+        firstName: json.firstName,
+        lastName: json.lastName,
+        email: json.email,
+        isLoading: false,
+      });
+    } catch (error) {
+      // this.setState({
+      //   // error: 'Failed to fetch profile data.',
+      // });
+    }
+  };
 
   checkLoggedIn = async () => {
     const loginToken = await AsyncStorage.getItem('sessionAuthToken');
@@ -40,10 +62,30 @@ export default class ProfileScreen extends Component {
   };
 
   render() {
-    return (
-      <View>
-        <Text>Profile Screen</Text>
-      </View>
-    );
+    if (this.state.isLoading) {
+      return (
+        <View>
+          <ActivityIndicator />
+        </View>
+      );
+    } else {
+      return (
+        <View>
+          <Text>Profile Screen</Text>
+          <Text>
+            FirstName:
+            {this.state.firstName}
+          </Text>
+          <Text>
+            LastName:
+            {this.state.lastName}
+          </Text>
+          <Text>
+            Email:
+            {this.state.email}
+          </Text>
+        </View>
+      );
+    }
   }
 }
